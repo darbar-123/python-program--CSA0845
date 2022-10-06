2eb98661121d85d61786152b1ba385fa7eab7754
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2022-10-06 14:54:28 +0530

    Add files via upload

---
+++ proper combinations.py
@@ -0,0 +1,7 @@
+def comb(l):
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                    if(i!=j and j!=k and k!=i):
+                        print(l[i],l[j],l[k])
+comb([5,5,5])
