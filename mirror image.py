cf4019204fe73d940e9bae4af72c553982b923ee
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2022-10-06 23:28:48 +0530

    Add files via upload

---
+++ mirror image.py
@@ -0,0 +1,10 @@
+def reverse(s):
+    num = " "
+    for i in s:
+        num = i + num
+    return num
+s = input("enter the number:")
+print("the mirror image is : ",end=" ")
+print(reverse(s))
+    
+    
---
+++ number of good pirs.py
@@ -0,0 +1,12 @@
+n=int(input("enter the number of elements"))
+l=[]
+count=0
+for i in range(n):
+    m=int(input())
+    l.append(m)
+print(l)
+for j in range(len(l)):
+    for k in range(j+1):
+        if(l[j]==l[k]):
+            count=count+1
+print(count-n)
