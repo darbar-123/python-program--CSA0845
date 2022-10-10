ef3326bafde7da59c61b54accd8e75e0e17f7d29
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2022-10-10 22:20:57 +0530

    Add files via upload

---
+++ binary to decimal6.22.py
@@ -0,0 +1,16 @@
+def into_decimal(data):
+    decimal = 0
+    j = 0
+    while data != 0:
+        decimal += int(data%10)*int(pow(2,j))
+        j += 1
+        data = data / 10
+    return decimal
+def into_octal(data):
+    return oct(into_decimal(data))
+data = input('Binary data :')
+ch = input('Enter either D/O : ')
+if ch=='D' or ch=='d':
+    print('Decimal form :',into_decimal(int(data)))
+elif ch=='O' or ch=='o':
+    print('Octal form :',into_octal(int(data)))
